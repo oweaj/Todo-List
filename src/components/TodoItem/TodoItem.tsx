7f3946fb33f620cbf71c50f892174d2850e8c837
oweaj <qwe1520156@naver.com>
2023-07-14 19:23:34 +0900

    [FEAT] Todoitem 완료 상태, 수정, 삭제 완료

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,28 +1,80 @@
-import { BsCheckCircleFill } from "react-icons/bs";
-import { AiOutlineMinusCircle } from "react-icons/ai";
+import React, { MouseEvent, useState } from "react";
 import { RxCross2 } from "react-icons/rx";
-import { useState } from "react";
+import { BsPencilFill, BsCheckLg } from "react-icons/bs";
+import { user } from "../../Atom/userAtom";
+import { useRecoilValue } from "recoil";
+import { collection, deleteDoc, getDocs, query, updateDoc, where } from "firebase/firestore";
+import { db } from "../../firebase";
+import { todoInfo } from "../../page/Todo/Todo";
 
 interface itemText {
-  item: string;
+  item: todoInfo;
 }
 
 const TodoItem = ({ item }: itemText) => {
-  const [icon, setIcon] = useState<"minus" | "check">("minus");
+  const [editMode, setEditMode] = useState<boolean>(false);
+  const [editValue, setEditValue] = useState<string>("");
+  const isUser = useRecoilValue(user);
 
-  const isToggle = () => {
-    setIcon(icon === "minus" ? "check" : "minus");
+  const isFinish = async (id: string, state: boolean) => {
+    const checkId = query(collection(db, `user/${isUser.uid}/todo`), where("id", "==", id));
+    const findFinish = await getDocs(checkId);
+
+    findFinish.forEach(async (doc) => {
+      await updateDoc(doc.ref, {
+        state,
+      });
+    });
   };
 
-  const isDelete = () => {
-    console.log("삭제");
+  const isEdit = async (id: string, todo: string | MouseEvent<HTMLOrSVGElement>) => {
+    const checkId = query(collection(db, `user/${isUser.uid}/todo`), where("id", "==", id));
+    const findUpdate = await getDocs(checkId);
+
+    findUpdate.forEach(async (doc) => {
+      await updateDoc(doc.ref, {
+        todo,
+      });
+    });
+    setEditMode(false);
+  };
+
+  const isDelete = async (id: string | MouseEvent<HTMLOrSVGElement>) => {
+    const checkId = query(collection(db, `user/${isUser.uid}/todo`), where("id", "==", id));
+    const findDelete = await getDocs(checkId);
+
+    findDelete.forEach(async (doc) => {
+      await deleteDoc(doc.ref);
+    });
   };
 
   return (
     <li className="relative w-full h-16 flex flex-row items-center gap-4 px-2 text-lg border-b border-gray-400">
-      {icon === "minus" ? <AiOutlineMinusCircle className="w-6 h-6 cursor-pointer" onClick={isToggle} /> : <BsCheckCircleFill className="w-6 h-6 text-blue-500 cursor-pointer" onClick={isToggle} />}
-      <span className={icon === "check" ? "text-gray-400" : ""}>{item}</span>
-      <RxCross2 className="w-6 h-6 absolute right-3 cursor-pointer" onClick={isDelete} />
+      <input
+        type="checkbox"
+        className="w-5 h-5"
+        onClick={(e) => {
+          const target = e.target as HTMLInputElement;
+          isFinish(item.id, target.checked);
+        }}
+        defaultChecked={item.state}
+      />
+      <span className={item.state ? "text-gray-400 line-through" : ""}>{editMode ? "" : item.todo}</span>
+      {editMode && <input type="text" className="w-2/3 h-9 p-2 border border-gray-400 rounded-lg" onChange={(e) => setEditValue(e.target.value)} />}
+      <div className="absolute right-3 flex flex-row items-center gap-4">
+        {editMode ? (
+          <button type="button" onClick={() => isEdit(item.id, editValue)}>
+            <BsCheckLg className="w-6 h-6" />
+          </button>
+        ) : (
+          <button type="button" onClick={() => setEditMode(true)} disabled={item.state}>
+            <BsPencilFill className="w-5 h-5" />
+          </button>
+        )}
+        <button type="button" onClick={() => isDelete(item.id)} disabled={item.state}>
+          <RxCross2 className="w-6 h-6" />
+        </button>
+      </div>
     </li>
   );
 };
