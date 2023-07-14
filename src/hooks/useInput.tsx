d8fd6ce3954e9551ad3d14071ed82becf015e4d8
oweaj <qwe1520156@naver.com>
2023-07-14 19:20:41 +0900

    [FEAT] 비밀번호 재확인 검사 및 회원가입 완료

--- src/hooks/useInput.tsx
+++ src/hooks/useInput.tsx
@@ -5,7 +5,7 @@ const useInput = () => {
   const [state, setState] = useState<boolean>(false);
   const emailCheck = /([\w-.]+)@(([\w-]+\.)+)([a-zA-Z]{2,4})(\]?)$/;
   const passWordCheck = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/;
-  const nickNameCheck = /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{3,8}$/;
+  // const nickNameCheck = /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{3,8}$/;
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { id, value } = e.target;
@@ -16,7 +16,7 @@ const useInput = () => {
     } else if (id === "password") {
       setState(passWordCheck.test(value) ? true : false);
     } else {
-      setState(nickNameCheck.test(value) ? true : false);
+      setState(false);
     }
   };
 
--- src/page/Signup/Signup.tsx
+++ src/page/Signup/Signup.tsx
@@ -1,17 +1,36 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState, MouseEvent } from "react";
 import { Input, Button } from "../../components/Items/Items";
-import Title from "../../components/Title/Title";
+import Title from "../../components/Header/Header";
 import useInput from "../../hooks/useInput";
+import { useNavigate } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { authService } from "../../firebase";
+import { useSetRecoilState } from "recoil";
+import { checkPW } from "../../Atom/userAtom";
 
 const Signup = () => {
   const [active, setActive] = useState<boolean>(false);
   const email = useInput();
   const password = useInput();
-  const nickName = useInput();
+  const pwCheck = useInput();
+  const passwordCheck = useSetRecoilState(checkPW);
+  const navigate = useNavigate();
+
+  const register = async (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    try {
+      await createUserWithEmailAndPassword(authService, email.data, password.data);
+      alert("회원가입 되었습니다.");
+      navigate("/");
+    } catch (error) {
+      alert("아이디를 다시 입력해주세요.");
+    }
+  };
 
   useEffect(() => {
-    email.state && password.state && nickName.state ? setActive(true) : setActive(false);
-  }, [email.state, password.state, nickName.state]);
+    passwordCheck(password.data === pwCheck.data);
+    email.state && password.state && password.data === pwCheck.data ? setActive(true) : setActive(false);
+  }, [email.state, password.state, pwCheck.data]);
 
   return (
     <div className="contentsStyle">
@@ -20,8 +39,8 @@ const Signup = () => {
         <div className="formGap">
           <Input title="이메일" id={"email"} value={email.data} state={email.state} onChange={email.onChange} />
           <Input title="비밀번호" id={"password"} value={password.data} state={password.state} onChange={password.onChange} />
-          <Input title="닉네임" id={"nickName"} value={nickName.data} state={nickName.state} onChange={nickName.onChange} />
-          <Button title="회원가입" id={"signup-button"} active={active} />
+          <Input title="비밀번호 재확인" id={"pwCheck"} value={pwCheck.data} state={pwCheck.state} onChange={pwCheck.onChange} />
+          <Button title="회원가입" id={"signup-button"} onClick={register} active={active} />
         </div>
       </form>
     </div>
