cd9212d418beb0c6c0a558f823ae47a2c7d26ebf
oweaj <qwe1520156@naver.com>
2023-07-14 19:28:14 +0900

    [CHORE] 주석제거

--- src/hooks/useInput.tsx
+++ src/hooks/useInput.tsx
@@ -5,7 +5,6 @@ const useInput = () => {
   const [state, setState] = useState<boolean>(false);
   const emailCheck = /([\w-.]+)@(([\w-]+\.)+)([a-zA-Z]{2,4})(\]?)$/;
   const passWordCheck = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/;
-  // const nickNameCheck = /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{3,8}$/;
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { id, value } = e.target;
