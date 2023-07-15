d53f157b95b1e8bf8a9229ccd5a291e7c92d44d0
oweaj <qwe1520156@naver.com>
2023-07-15 11:42:55 +0900

    [FEAT] 구글 로그인 추가

--- src/page/Signin/Signin.tsx
+++ src/page/Signin/Signin.tsx
@@ -3,7 +3,7 @@ import { Input, Button } from "../../components/Items/Items";
 import { Link, useNavigate } from "react-router-dom";
 import Title from "../../components/Header/Header";
 import useInput from "../../hooks/useInput";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { authService } from "../../firebase";
 
 const Signin = () => {
@@ -23,6 +23,17 @@ const Signin = () => {
     }
   };
 
+  const handleGoogleLogin = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      await signInWithPopup(authService, provider);
+      alert("로그인 되었습니다.");
+      navigate("/todo");
+    } catch (error) {
+      alert("로그인에 실패했습니다.");
+    }
+  };
+
   useEffect(() => {
     email.state && password.state ? setActive(true) : setActive(false);
   }, [email.state, password.state]);
@@ -35,9 +46,15 @@ const Signin = () => {
           <Input title="이메일" id={"email"} value={email.data} state={email.state} onChange={email.onChange} />
           <Input title="비밀번호" id={"password"} value={password.data} state={password.state} onChange={password.onChange} />
           <Button title="로그인" id={"signin-button"} onClick={login} active={active} />
-          <Link to="/signup" className="flex items-center justify-center w-96 h-12 rounded-lg text-lg text-white bg-blue-400">
-            회원가입
-          </Link>
+          <div className="flex flex-row items-center justify-center text-center">
+            <button type="button" className="w-28" onClick={handleGoogleLogin}>
+              Google 로그인
+            </button>
+            <span className="mx-6">|</span>
+            <Link to="/signup" className="w-28">
+              회원가입
+            </Link>
+          </div>
         </div>
       </form>
     </div>
